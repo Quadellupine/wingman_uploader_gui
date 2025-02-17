8d94845fc423b1ea5e7e8a3f85323511a0cbcfb0
Nicky Nicky <nickyistoxic@gmail.com>
2025-02-17 18:01:59 +0100

    Dealing with working dir problems because of the release structure

---
+++ LogFC
@@ -0,0 +1,1 @@
+# LogFC-Testing
--- gui_version/flamebot_integration.py
+++ gui_version/flamebot_integration.py
@@ -5,7 +5,8 @@ from utils import get_current_time, confirm_download
 import zipfile
 import subprocess
 import platform
-
+import requests
+import shutil
 
 # please make it stop
 def bill_gates_special():
@@ -40,17 +41,47 @@ def flamebot_input(pos_x, pos_y,flame_lang, flame_output_path):
         return
     logs = split_links(logs)
     run_flamebot(logs,flame_lang, flame_output_path)
-    
-def run_flamebot(logs,flame_lang,flame_output_path):
-    BrizehUrl = "https://github.com/Brizeh/LogFC/archive/refs/heads/main.zip"
-    while not os.path.isdir("LogFC"):
+
+def flatten_directory(path):
+    folders = os.listdir(path)
+    for item in folders:
+        for filename in os.listdir(os.path.join(path, item)):
+            new_path = os.path.join(path, filename)
+            shutil.move(os.path.join(path,item, filename), os.path.join(path))
+        if os.path.isdir(os.path.join(path,item)):
+            shutil.rmtree(os.path.join(path,item))
+
+def asset_management():
+    # This function manages the LogFC assets
+    BrizehUrl = "https://api.github.com/repos/Brizeh/LogFC/releases/latest"
+    query = requests.get(BrizehUrl).json()
+    version = query["name"]
+    if os.path.isdir("LogFC-main") and os.path.isfile("LogFC-main/version.txt"):
+        with open("LogFC-main/version.txt", "r") as file:
+            installed_version = file.read()
+        if version == installed_version:
+            print(get_current_time(),"LogFC installed and up to date with latest release!")
+            return()
+        else:
+            print(get_current_time(), "Current version: ",installed_version, " Latest version: ", version)
+            print(get_current_time(), "Deleting current install and downloading latest version")
+    else:
+        print(get_current_time(), "No (proper) LogFC installation, pulling latest release")
+    DownloadUrl = "https://api.github.com/repos/Brizeh/LogFC/zipball/logfc"
+    if os.path.isdir("LogFC-main"):
+        shutil.rmtree("LogFC-main")
+    while not os.path.isdir("LogFC-main"):
         print(get_current_time(), "LogFC is missing, downloading it")
         confirm_download("Brizeh's Flamebot", "https://github.com/Brizeh/LogFC")
-        wget.download(BrizehUrl, "LogFC.zip")
+        wget.download(DownloadUrl, "LogFC.zip")
         with zipfile.ZipFile("LogFC.zip", "r") as zip_ref:
-            zip_ref.extractall("LogFC")
+            zip_ref.extractall("LogFC-main")
         os.remove("LogFC.zip")
-    
+    flatten_directory("LogFC-main")
+    with open("LogFC-main/version.txt", "w") as file:
+        file.write(version)
+
+def run_flamebot(logs,flame_lang,flame_output_path):    
     flame_lang = '"'+flame_lang+'"'
     change_language(flame_lang)
     os.remove("LogFC/LogFC-main/src/input_logs.txt")
@@ -72,3 +103,6 @@ def run_flamebot(logs,flame_lang,flame_output_path):
     outfile = flame_output_path+"/output.txt"
     with open(outfile, 'w', encoding='utf-8') as file:
         file.write(flameoutput.stdout)
+
+asset_management()
+#flatten_directory("LogFC")
